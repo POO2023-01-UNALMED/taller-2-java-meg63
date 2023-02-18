f0f1ca1ba046cb5d3d4bd4376ef71ee8bd55fc10
meg63 <mechavarriac@unal.edu.co>
2023-02-18 11:34:30 -0500

    Update Auto.java

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -20,7 +20,7 @@ public class Auto {
 		return cantidad;
 		
 	}
-	public String verficarIntegridad() {
+	public String verificarIntegridad() {
 		if(registro==motor.registro) {
 			for(int i=0;i>asientos.length;i++) {
 				if(asientos[i]!=null) {
